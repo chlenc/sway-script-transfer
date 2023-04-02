4a3c583c3532d744de69f0e52bda0870adb1d9cc
alexey <alexnagornnyy.an@gmail.com>
2023-04-02 13:08:13 +0100

    :tada: done

--- src/main.sw
+++ src/main.sw
@@ -1,5 +1,6 @@
 script;
 use std::token::transfer_to_address;
+use std::constants::BASE_ASSET_ID;
 fn main(amount: u64, asset: ContractId, bob_address: Address) {
     transfer_to_address(amount, asset, bob_address);
 }
--- tests/harness.rs
+++ tests/harness.rs
@@ -1,6 +1,9 @@
 use std::str::FromStr;
 
-use fuels::{prelude::*, tx::ContractId};
+use fuels::{
+    prelude::*,
+    tx::{ContractId, Output},
+};
 
 abigen!(Script(
     name = "Script",
@@ -8,7 +11,7 @@ abigen!(Script(
 ));
 #[tokio::test]
 async fn main() {
-    let mint_amout = 1_000_000_000;
+    let mint_amout = 1_000_000_000; //1 eth
 
     let wallets_config = WalletsConfig::new(Some(2), Some(1), Some(mint_amout));
     let wallets = launch_custom_provider_and_get_wallets(wallets_config, None, None).await;
@@ -16,7 +19,7 @@ async fn main() {
     let alice = wallets[0].clone();
     let alice_balance = alice.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
     let bob = wallets[1].clone();
-    let bob_balance = alice.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
+    let bob_balance = bob.get_asset_balance(&BASE_ASSET_ID).await.unwrap();
 
     assert_eq!(alice_balance, mint_amout);
     assert_eq!(bob_balance, mint_amout);
@@ -24,18 +27,26 @@ async fn main() {
     let amount0 = mint_amout / 2;
     let contract_id = ContractId::from_str(BASE_ASSET_ID.to_string().as_str()).unwrap();
     let instance = Script::new(alice.clone(), "out/debug/script_transfer.bin");
+    let outputs = vec![Output::Variable {
+        amount: amount0,
+        to: Address::from(bob.address()),
+        asset_id: BASE_ASSET_ID,
+    }];
     let receipts = instance
         .main(amount0, contract_id, Address::from(bob.address()))
         .tx_params(TxParameters::default().set_gas_price(1))
+        .with_outputs(outputs)
         .call()
         .await;
 
-    dbg!(&receipts);
+    if receipts.is_err() {
+        dbg!(&receipts);
+    }
 
     receipts.unwrap();
 
     assert_eq!(
-        alice.get_asset_balance(&BASE_ASSET_ID).await.unwrap(),
+        bob.get_asset_balance(&BASE_ASSET_ID).await.unwrap(),
         bob_balance + amount0
     );
 }
